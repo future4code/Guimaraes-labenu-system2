14f5f838612412011a5dae57996af2d99bf1a63f
Robson Rodrigues Ribeiro <projetos.r@hotmail.com>
2022-07-22 20:20:15 -0300

    criado class estudante

--- src/classes/estudante.ts
+++ src/classes/estudante.ts
@@ -1,16 +1,39 @@
+import { IEstudante } from "../interfaces/IEstudante";
 import { Pessoa } from "./pessoa";
+import { EstudanteServices } from "../core/services/EstudanteServices"
 
 
-export class Estudante extends Pessoa {
-    private hobbies: string []
+export class Estudante extends EstudanteServices implements IEstudante {
+    public id?: number
+    public nome?: string
+    public email?: string
+    public data_nasc?: number
+    public turma_id?: string
+    public hobbies?: string
+    
 
-
-    constructor(id: string, name: string, email: string, data_nasc: number, turma_id: string, hobbies: []) {
-        super(id, name, email, data_nasc, turma_id)
-
+    constructor(id?: number, nome?: string, email?: string, data_nasc?: number, turma_id?: string, hobbies?: string) {
+        super()
+        this.id = id,
+        this.nome = nome,
+        this.email = email,
+        this.data_nasc = data_nasc,
+        this.turma_id = turma_id,
         this.hobbies = hobbies
-        
+    }
+    async criarEstudante(estudante: Estudante): Promise<object> {
+        return await this.createEstudante(estudante)
     }
 
-    
+    async buscarEstudanteAtivos(): Promise<[]> {
+        return await this.all()
+    }
+
+    mudarEstudanteModulo(idEstudanteAntigo: string, idEstudanteNovo: string): boolean {
+        throw new Error("Method not implemented.");
+    }
+
+    async buscarEstudantePorNome(): Promise<[]>{
+        return await this.all()
+    }
 }
